3d145728bf11a8a5f7355bf1724ca4af2f28b42b
Jerry <hwy0127@gmail.com>
2021-09-18 22:38:24 +0800

    'backup'

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp')
 const cleanCSS = require('gulp-clean-css')
 const htmlmin = require('gulp-html-minifier-terser')
 const htmlclean = require('gulp-htmlclean')
-const imagemin = require('gulp-imagemin')
+// const imagemin = require('gulp-imagemin')
 const terser = require('gulp-terser');
 
 // minify js
@@ -38,18 +38,18 @@ gulp.task('minify-html', () => {
 })
 
 // 壓縮 public/uploads 目錄內圖片
-gulp.task('minify-images', async () => {
-  gulp.src('./public/img/**/*.*')
-    .pipe(imagemin({
-      optimizationLevel: 5, // 類型：Number  預設：3  取值範圍：0-7（優化等級）
-      progressive: true, // 類型：Boolean 預設：false 無失真壓縮jpg圖片
-      interlaced: false, // 類型：Boolean 預設：false 隔行掃描gif進行渲染
-      multipass: false // 類型：Boolean 預設：false 多次優化svg直到完全優化
-    }))
-    .pipe(gulp.dest('./public/img'))
-})
+// gulp.task('minify-images', async () => {
+//   gulp.src('./public/img/**/*.*')
+//     .pipe(imagemin({
+//       optimizationLevel: 5, // 類型：Number  預設：3  取值範圍：0-7（優化等級）
+//       progressive: true, // 類型：Boolean 預設：false 無失真壓縮jpg圖片
+//       interlaced: false, // 類型：Boolean 預設：false 隔行掃描gif進行渲染
+//       multipass: false // 類型：Boolean 預設：false 多次優化svg直到完全優化
+//     }))
+//     .pipe(gulp.dest('./public/img'))
+// })
 
 // 執行 gulp 命令時執行的任務
 gulp.task('default', gulp.parallel(
-  'compress', 'minify-css', 'minify-html', 'minify-images'
+  'compress', 'minify-css', 'minify-html'
 ))
