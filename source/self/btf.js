fe0578e8920f149d7c576aa2580b07f8a1d9e3d6
Jerry <hwy0127@gmail.com>
2023-07-03 01:41:28 +0800

    update

--- source/self/btf.js
+++ source/self/btf.js
@@ -1,16 +1,18 @@
-const isIncludeEN = (item) => {
-  const key = '/en/'
-  return item.includes(key)
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const isIncludeEN = item => {
+    const key = '/en/'
+    return item.includes(key)
+  }
 
-if (isIncludeEN(window.location.href)) {
-  document.querySelectorAll('a[href^="https://butterfly.js.org"]').forEach(item => {
-    if (!isIncludeEN(item.href)) {
+  if (isIncludeEN(window.location.href)) {
+    document.querySelectorAll('a[href^="https://butterfly.js.org"]').forEach(item => {
+      if (!isIncludeEN(item.href)) {
+        item.setAttribute("data-pjax-state", 'external')
+      }
+    })
+  } else {
+    document.querySelectorAll('a[href^="/en/"]').forEach(item => {
       item.setAttribute("data-pjax-state", 'external')
-    }
-  })
-} else {
-  document.querySelectorAll('a[href^="/en/"]').forEach(item => {
-    item.setAttribute("data-pjax-state", 'external')
-  })
-}
+    })
+  }
+})
