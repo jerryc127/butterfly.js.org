323f6041589fb715e38a9a49bc6cb87311c9abb5
Jerry <hwy0127@gmail.com>
2023-07-03 01:36:57 +0800

    update

--- source/self/btf.js
+++ source/self/btf.js
@@ -1,37 +1,16 @@
-// document.addEventListener('pjax:send', function (e) {
+const isIncludeEN = (item) => {
+  const key = '/en/'
+  return item.includes(key)
+}
 
-//   const checkInclude = (item1, item2) => {
-//     const key = '/en/'
-//     const item1Has = item1.includes(key)
-//     const item2Has = item2.includes(key)
-//     return item1Has && !item2Has || !item1Has && item2Has
-//   }
-//   console.log(e);
-
-  
-//   const src = window.location.href
-//   const target = e.triggerElement.href || ''
-
-//   if (checkInclude(src, target)) {
-//     e.preventDefault()
-//     e.stopImmediatePropagation()
-//     e.stopPropagation()
-//     console.log(e);
-//     console.log('preventDefault');
-//     window.location.href = target
-//     return
-//   }
-// })
-
-// document.addEventListener('pjax:success', function (e) {
-//   console.log('pjax:success');
-// })
-
-// window.openNew = function (url) {
-//   window.location.href = url
-// }
-
-document.querySelectorAll('a[href^="/en/"]').forEach(item => {
-  item.setAttribute("data-pjax-state", 'load')
-})
-
+if (isIncludeEN(window.location.href)) {
+  document.querySelectorAll('a[href^="https://butterfly.js.org"]').forEach(item => {
+    if (!isIncludeEN(item.href)) {
+      item.setAttribute("data-pjax-state", 'external')
+    }
+  })
+} else {
+  document.querySelectorAll('a[href^="/en/"]').forEach(item => {
+    item.setAttribute("data-pjax-state", 'external')
+  })
+}
