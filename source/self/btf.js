517b53631e7f99a1204cf2be40fd808105afd5c0
Jerry Wong <hwy0127@gmail.com>
2023-07-04 01:50:28 +0800

    Update btf.js

--- source/self/btf.js
+++ source/self/btf.js
@@ -14,7 +14,7 @@
   }
   
   const replaceHref = item => {
-    item.href = `javascript:fullLoadPage(${item.href})`
+    item.href = `javascript:fullLoadPage(“${item.href}”)`
   }
     
   const eventFn = (ele) => {
